a1fe2daeb7e63511064a56027928dc799a8edafe
Danil <makarovdanil289@gmail.com>
2025-04-14 23:25:58 +0500

    first commit

---
+++ src/components/Projects.jsx
@@ -0,0 +1,111 @@
+import {
+    Container,
+    Row,
+    Col,
+    Nav,
+    TabContainer,
+    TabContent,
+    Tab,
+} from "react-bootstrap";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+
+import { ProjectCard } from "./ProjectCard";
+
+export const Projects = () => {
+    const projects = [
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg1,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg2,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg3,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg1,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg2,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg3,
+        },
+    ];
+
+    return (
+        <section className="project" id="project">
+            <Container>
+                <Row>
+                    <Col>
+                        <h2>Projects</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Tempore sequi ullam numquam voluptate nisi
+                            iste eaque nihil, magni, aspernatur nesciunt
+                            provident reprehenderit incidunt! Quo omnis harum
+                            eaque eos beatae aliquid.
+                        </p>
+                        <TabContainer
+                            id="projects-tabs"
+                            defaultActiveKey="first"
+                        >
+                            <Nav
+                                variant="pills"
+                                defaultActiveKey="/home"
+                                className="nav-pills mb-5 justify-content-center"
+                                id="pills-tab"
+                            >
+                                <Nav.Item>
+                                    <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="third">Tab 2</Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                            <TabContent>
+                                <Tab.Pane eventKey="first">
+                                    <Row>
+                                        {projects.map((project, index) => {
+                                            return (
+                                                <ProjectCard
+                                                    key={index}
+                                                    {...project}
+                                                />
+                                            );
+                                        })}
+                                    </Row>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="second">
+                                    Lorem Ipsum
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="third">
+                                    Lorem Ipsum
+                                </Tab.Pane>
+                            </TabContent>
+                        </TabContainer>
+                    </Col>
+                </Row>
+            </Container>
+            <img className="background-image-right" src={colorSharp2} />
+        </section>
+    );
+};
